83d13ba9b634abd568d91e02c65eb4f0f16cf94a
Rachael Otto <rachael.g.otto@gmail.com>
2023-11-14 11:46:14 -0800

    add Tailwind CSS

--- congressle-vite/src/App.jsx
+++ congressle-vite/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
-import './App.css'
 import { APP_TITLE, WELCOME_MSG } from './constants/lang'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>{APP_TITLE}</h1>
+      <h1 className="font-bold">{APP_TITLE}</h1>
       <p>{WELCOME_MSG}</p>
-      <div className="game">
+      <div>
         
       </div>
     </>
